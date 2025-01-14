51786078b35f98347f68791e37e330714540a703
Gabry2504 <Bomber0Tennis0@gmail.com>
2025-01-14 11:53:25 +0100

    Implements test, class and deploy file

--- Operation/Sum.cs
+++ Operation/Sum.cs
@@ -1,5 +1,18 @@
 ﻿namespace Operation;
 public class Sum
 {
-
+    public int Calculate(int a, int b)
+    {
+        try
+        {
+            checked
+            {
+                return a + b;
+            }
+        }
+        catch (OverflowException)
+        {
+            throw new ArgumentException("The result exceeds the allowed range for integers.");
+        }
+    }
 }
--- OperationTest/SumTest.cs
+++ OperationTest/SumTest.cs
@@ -1,10 +1,28 @@
+using Operation;
+
 namespace OperationTest;
 
 public class SumTest
 {
-    [Fact]
-    public void Test1()
+    private readonly Sum _sum;
+    public SumTest()
     {
+        _sum = new Sum();
+    }
+    [Fact]
+    public void Calculate_ValidInputs_ReturnsCorrectSum()
+    {
+        int a = 10;
+        int b = 20;
+        int result = _sum.Calculate(a, b);
+        Assert.Equal(30, result);
+    }
 
+    [Fact]
+    public void Calculate_Overflow_ThrowsArgumentException()
+    {
+        int a = int.MaxValue;
+        int b = 1;
+        Assert.Throws<ArgumentException>(() => _sum.Calculate(a, b));
     }
 }
